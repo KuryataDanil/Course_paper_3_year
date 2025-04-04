ba232dfc44d91e74e45865880ddea6dea1f4746a
d.kuryata <d.kuryata@tbank.ru>
2025-04-04 22:36:12 +0300

    refactor code and add save model method

---
+++ main.py
@@ -0,0 +1,12 @@
+import train_and_save_model
+import test_model
+
+def main():
+    print("start learning")
+    train_and_save_model()
+    print("start testing")
+    test_model()
+
+
+if __name__ == "__main__":
+    main()
